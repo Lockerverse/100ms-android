719579c2bbc8aa364856bc1eee23d98d884cd35f
pratimmallick <pratim@100ms.live>
2025-07-25 13:48:46 +0530

    Update to 16kb enabled HMS SDK

--- room-kit/src/test/java/live/hms/roomkit/ExampleUnitTest.kt
+++ room-kit/src/test/java/live/hms/roomkit/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package live.hms.roomkit
 
-import hms.junit.Test
+import org.junit.Test
 
-import hms.junit.Assert.*
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
